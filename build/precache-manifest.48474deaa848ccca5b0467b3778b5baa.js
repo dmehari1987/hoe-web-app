556f44e8c991251e4e1ec4ca912a55b1c116cc11
Dani <meharidaniel1111@gmail.com>
2020-04-17 23:12:57 +0200

    first phase afteter correcting deploy error

---
+++ build/precache-manifest.48474deaa848ccca5b0467b3778b5baa.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a4a9f170d72f887b454d18f6c41f71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ce5e0246a9da559a462",
+    "url": "/static/css/2.ef179ede.chunk.css"
+  },
+  {
+    "revision": "d7dc9b9a52d706b6ddfd",
+    "url": "/static/css/main.88a9501d.chunk.css"
+  },
+  {
+    "revision": "6ce5e0246a9da559a462",
+    "url": "/static/js/2.db6ac5cb.chunk.js"
+  },
+  {
+    "revision": "199e7af80bee82ee58d873d2d4aa946f",
+    "url": "/static/js/2.db6ac5cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7dc9b9a52d706b6ddfd",
+    "url": "/static/js/main.b568f5fe.chunk.js"
+  },
+  {
+    "revision": "6f7aed2a9197788a1e1d",
+    "url": "/static/js/runtime-main.820247e6.js"
+  },
+  {
+    "revision": "0ce5cb1cd71360763b59c153069de45c",
+    "url": "/static/media/Classroom.0ce5cb1c.jfif"
+  },
+  {
+    "revision": "a4e8f43a03f883d46b70d614bb4193b0",
+    "url": "/static/media/Discussion.a4e8f43a.jfif"
+  },
+  {
+    "revision": "03684d7dd07e726f12154af3bc860ede",
+    "url": "/static/media/Outdoor.03684d7d.jfif"
+  },
+  {
+    "revision": "b2a6b324e9f932cd4e20868ab737c986",
+    "url": "/static/media/ceo.b2a6b324.jpg"
+  },
+  {
+    "revision": "cce5b91a9a012dc8f980b30c260166ce",
+    "url": "/static/media/chairman.cce5b91a.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "f391e657f9cabe91f018ca14e340d693",
+    "url": "/static/media/logo.f391e657.svg"
+  },
+  {
+    "revision": "63be9d6d42e29b1cd1533677a9cfed65",
+    "url": "/static/media/parentchairwoman.63be9d6d.png"
+  },
+  {
+    "revision": "186fb6c9f8310c17ccf2de16633b026f",
+    "url": "/static/media/principal.186fb6c9.png"
+  }
+]);
